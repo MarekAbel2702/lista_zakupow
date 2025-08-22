0d9d627d913372c33a674db0b712ee39c018b87c
Marek Abel <marek-abel@wp.pl>
2025-08-22 23:25:19 +0200

    Dodanie interfejsu IShoppingRepository do Domain

---
+++ Lista zakupów/Domain/Interfaces/IShoppingItemRepository.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IShoppingItemRepository
+    {
+        Task<ShoppingItem> GetByIdAsync(int id, CancellationToken ct = default);
+        Task<IReadOnlyList<ShoppingItem>> GetAllAsync(
+            bool onlyNotPurchased = false, 
+            CancellationToken ct = default);
+        Task<PagedResult<ShoppingItem>> SearchAsync(
+            ItemSearchCriteria criteria,
+            CancellationToken ct = default);
+        Task AddAsync(ShoppingItem item, CancellationToken ct = default);
+        Task UpdateAsync(ShoppingItem item, CancellationToken ct = default);
+        Task DeleteAsync(int id, CancellationToken ct = default);
+        Task<bool> ExistByNameAsync(string normalizedName,  CancellationToken ct = default);
+    }
+}
